757bb2a72f9551e36c3fe2d490a45124ed827336
777jiexi <103366538+777jiexi@users.noreply.github.com>
2022-06-13 18:49:02 +0800

    Create sub.php

---
+++ api/sub.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: text/plain;charset=utf-8');
+if(strstr($_SERVER['QUERY_STRING'],"target=") && strstr($_SERVER['QUERY_STRING'],"url=")){
+    $result = file_get_contents("http://42.157.128.25:25500/sub?".$_SERVER['QUERY_STRING']);
+    $result = str_replace("wulabing_ww1.u3.gs","香港1",$result);
+    $result = str_replace("wulabing_www.u3.gs","香港2",$result);
+    echo($result);exit;
+}else{
+    echo("请输入参数");exit;
+}
