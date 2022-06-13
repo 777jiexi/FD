3df159497d21e8eaaa89421afa9722b5c9e27db4
777jiexi <103366538+777jiexi@users.noreply.github.com>
2022-06-13 10:20:08 +0800

    1
    1

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
-header('Content-Type: text/plain;charset=utf-8'); //设置内容长度
-if(!strstr($_SERVER['QUERY_STRING'],"target=") || !strstr($_SERVER['QUERY_STRING'],"url=")){
+header('Content-Type: text/plain;charset=utf-8');
+if(strstr($_SERVER['QUERY_STRING'],"target=") && strstr($_SERVER['QUERY_STRING'],"url=")){
+    echo(file_get_contents("http://42.157.128.25:25500/sub?".$_SERVER['QUERY_STRING']));exit;
+}else{
     echo("请输入参数");exit;
 }
-echo(file_get_contents("http://42.157.128.25:25500/sub?".$_SERVER['QUERY_STRING']));
