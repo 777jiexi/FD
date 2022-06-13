250e3862c9e32c12e064e4346d537e8ea1c327d3
777jiexi <103366538+777jiexi@users.noreply.github.com>
2022-06-13 10:14:00 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,6 @@
 <?php
-error_repoting(0);
 header('Content-Type: text/plain;charset=utf-8'); //设置内容长度
-if($_GET['target']=="" || $_GET['url']==""){
+if(!strstr($_SERVER['QUERY_STRING'],"target=") || !strstr($_SERVER['QUERY_STRING'],"url=")){
     echo("请输入参数");exit;
 }
 echo(file_get_contents("http://42.157.128.25:25500/sub?".$_SERVER['QUERY_STRING']));
